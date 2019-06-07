a2e1447365168f12f6300cb2013e4fb3a7adc344
mosi <momkdh2@naver.com>
2019-06-08 05:02:14 +0900

    loss 부분 수정 필요

--- modeling.py
+++ modeling.py
@@ -4,26 +4,35 @@ import argparse
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
+from torchvision.transforms import transforms
 
 class Generator(nn.Module):
     def __init__(self, args):
         super(Generator, self).__init__()
         self.layer1 = nn.Linear(args.latent_z_dim, 128)
         self.relu1 = nn.ReLU()
-        self.layer2 = nn.Linear(128, 784)
+        self.layer2 = nn.Linear(128, 256)
+        self.relu2 = nn.ReLU()
+        self.layer3 = nn.Linear(256, 512)
+        self.relu3 = nn.ReLU()
+        self.layer4 = nn.Linear(512, 784)
 
     def forward(self, x):
         x = self.layer1(x)
         x = self.relu1(x)
         x = self.layer2(x)
+        x = self.relu2(x)
+        x = self.layer3(x)
+        x = self.relu3(x)
+        x = self.layer4(x)
         return x
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
-        self.layer1 = nn.Linear(784, 128)
+        self.layer1 = nn.Linear(784, 256)
         self.relu1 = nn.ReLU()
-        self.layer2 = nn.Linear(128, 1)
+        self.layer2 = nn.Linear(256, 1)
         self.sigmoid1 = nn.Sigmoid()
 
     def forward(self, x):
@@ -47,43 +56,48 @@ if __name__ == "__main__":
     parser.add_argument('--epochs', type=int, default=25)
     parser.add_argument('--latent_z_dim', type=int, default=100)
     args = parser.parse_args()
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     ## load dataset
     train_images, train_labels = torch.load(os.path.join(os.getcwd(), "preprocess/train.pt"))
-    train_images_loader = DataLoader(train_images, batch_size=args.batch_size)
+    train_images = (train_images - torch.mean(train_images))/torch.std(train_images)
+    train_images_loader = DataLoader(train_images.to(device=args.device), batch_size=args.batch_size)
 
     ## modeling
     normal_distribution = torch.distributions.normal.Normal(loc=torch.tensor(0.), scale=torch.tensor(1.))
-    G = Generator(args)
-    D = Discriminator()
+    G = Generator(args).to(device=args.device)
+    D = Discriminator().to(device=args.device)
 
     optimizer_G = torch.optim.Adam(G.parameters(), lr=0.0002)
     optimizer_D = torch.optim.Adam(D.parameters(), lr=0.0002)
-    loss_function = torch.nn.BCELoss()
+    loss_function = torch.nn.BCELoss().to(device=args.device)
 
     ## training
     for epoch in range(args.epochs):
         g_loss_list = []
         d_loss_list = []
         for sample in train_images_loader:
-            z = normal_distribution.sample(sample_shape=torch.Size([args.batch_size, args.latent_z_dim]))
+            z = normal_distribution.sample(sample_shape=torch.Size([args.batch_size, args.latent_z_dim])).to(device=args.device)
             generated_image = G(z)
 
             g_loss = loss_function((torch.tensor(1.) - D(generated_image)), torch.ones_like(D(generated_image)))
+            # g_loss = torch.mean(torch.log(torch.tensor(1.) - D(generated_image)))
             g_loss_list.append(g_loss.item())
             optimizer_G.zero_grad()
             g_loss.backward()
             optimizer_G.step()
 
-            d_loss_fake = loss_function(D(generated_image).detach(), torch.ones_like(D(generated_image)))
+            d_loss_fake = loss_function((torch.tensor(1.) - D(generated_image).detach()), torch.ones_like(D(generated_image)))
             d_loss_real = loss_function(D(sample.view(-1, 784)), torch.ones_like(D(sample.view(-1, 784))))
+            # d_loss_fake = torch.mean(-torch.log(torch.tensor(1.) - D(generated_image).detach()))
+            # d_loss_real = torch.mean(-torch.log(D(sample.view(-1, 784))))
             d_loss = (d_loss_fake + d_loss_real) / 2
             d_loss_list.append(d_loss.item())
             optimizer_D.zero_grad()
             d_loss.backward()
             optimizer_D.step()
 
-        z = normal_distribution.sample(sample_shape=torch.Size([25, args.latent_z_dim]))
+        z = normal_distribution.sample(sample_shape=torch.Size([25, args.latent_z_dim])).to(device=args.device)
         generated_image = G(z)
         print("epoch :", epoch, ",\t g_loss :", sum(g_loss_list), ",\t d_loss :", sum(d_loss_list))
         save_images(generated_image, epoch)

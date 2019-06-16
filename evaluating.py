182996f4f8d54e5dbbf53e71ef7de527a3fcfc37
mosi <momkdh2@naver.com>
2019-06-16 21:24:20 +0900

    annotating

--- evaluating.py
+++ evaluating.py
@@ -11,20 +11,25 @@ if __name__=="__main__":
     args = parser.parse_args()
     args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    # load model
     G = Generator(args).to(device=args.device)
     D = Discriminator().to(device=args.device)
 
+    # load pre-trained parameter
     G.load_state_dict(torch.load(os.path.join(os.getcwd(), "saved_model/generator")))
     D.load_state_dict(torch.load(os.path.join(os.getcwd(), "saved_model/discriminator")))
 
+    # define noise distribution
     normal_distribution = torch.distributions.normal.Normal(loc=torch.tensor(0.), scale=torch.tensor(1.))
 
+    # make save folder
     saved_folder = os.path.join(os.getcwd(), "evaluated_image")
     try:
         os.mkdir(saved_folder)
     except FileExistsError as e:
         pass
 
+    # generate & save image
     for i in range(args.num_images):
         z = normal_distribution.sample(sample_shape=torch.Size([1, args.latent_z_dim])).to(device=args.device)
         save_image(G(z), saved_folder + '/evaluated_' + str(i+1) + ' images.png')
